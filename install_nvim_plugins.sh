8c3aedd040f58fe5677e942c12a4cf9efca1f2ec
Adriano Moreira <adri3d@gmail.com>
2019-09-29 11:52:16 -0300

    improve install nvim plugins

--- install_nvim_plugins.sh
+++ install_nvim_plugins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c :PlugInstall
+nvim -c :PlugInstall -c :qa!
