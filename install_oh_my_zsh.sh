f4bced5a2c74afba18926ff20dc5a0c308a3b82d
Adriano Moreira <adri3d@gmail.com>
2019-12-04 22:06:09 -0300

    mkdir playground

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh
