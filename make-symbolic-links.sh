48075047e9f600ee8d9c35c94f4d28720147917a
Adriano Moreira <adri3d@gmail.com>
2021-06-19 11:29:35 -0300

    add .bin with jetbrains launchers

--- make-symbolic-links.sh
+++ make-symbolic-links.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -ex
+BASE=`dirname $(readlink -f $0)`
+echo $BASE
 
-ln -s -f `pwd`/.vimrc_base.vim  ~/.vimrc_base.vim 
-ln -s -f `pwd`/.vimrc  ~/.vimrc
-ln -s -f `pwd`/.ideavimrc  ~/.ideavimrc
+ln -s -f $BASE/.vimrc_base.vim  ~/.vimrc_base.vim 
+ln -s -f $BASE/.vimrc  ~/.vimrc
+ln -s -f $BASE/.ideavimrc  ~/.ideavimrc
 
 #nvim
-mkdir -p ~/.config
-ln -s -f `pwd`/.config/nvim  ~/.config
+mkdir -p ~/tools
+ln -s -f $BASE/.bin  ~/tools
 
 #tmux
-ln -s -f `pwd`/.tmux.conf ~/.tmux.conf
+ln -s -f $BASE/.tmux.conf ~/.tmux.conf
 
