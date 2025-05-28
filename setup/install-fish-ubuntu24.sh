5f5bac0b809cec9548cbd6c4ffb2678a46f7a605
Adriano Moreira <adri3d@gmail.com>
2025-05-28 07:22:55 -0300

    Update install-fish-ubuntu24.sh

--- setup/install-fish-ubuntu24.sh
+++ setup/install-fish-ubuntu24.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+# install from PPA
 sudo add-apt-repository ppa:fish-shell/release-4
 sudo apt update -y
+sudo apt install fish -y
+
+# set to login shell
 sudo apt install fish -y
